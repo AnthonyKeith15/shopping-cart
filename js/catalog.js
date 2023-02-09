e0791262da3db79010b7732db028196b4db0df01
Abshir Ibrahim <abshir.ibrahim@outlook.com>
2023-02-09 15:22:23 -0800

    lab14

--- js/catalog.js
+++ js/catalog.js
@@ -9,9 +9,13 @@ state.cart = new Cart([]);
 // (the things in the state.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //DONE: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in state.allProducts) {
+    let option = document.createElement('option');
+    option.textContent = state.allProducts[i].name;
+    option.value = state.allProducts[i].name;
+    selectElement.appendChild(option);
 
   }
 
@@ -22,18 +26,28 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
+  // : Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
   state.cart.saveToLocalStorage();
   state.cart.updateCounter();
   updateCartPreview();
+  
 
 }
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
+  // TODO: suss out the item picked from the select list
+  const selectElement = document.getElementById('items').value;
+
+  // TODO: get the quantity
+  const quantity = document.getElementById('quantity').value;
+
+  // TODO: using those, add one item to the Cart
+  
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
